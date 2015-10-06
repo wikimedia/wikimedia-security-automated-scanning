17435dc9cf8bd21e0a475830ac8a07145ffbf418
Darian Anthony Patrick <dpatrick@wikimedia.org>
2015-10-06 16:58:50 -0700

    Update cron script to reference config
    Change-Id: I4cf48daca1d8ae435919a1975412dbc082f37ae4

--- bin/scan_from_cron.sh
+++ bin/scan_from_cron.sh
@@ -1,23 +1,32 @@
 #!/bin/bash
 
-export PATH=~/.gem/ruby/2.0.0/bin:~/perl5/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-export PERL5LIB=/home/dpatrick/perl5/lib/perl5
-export GEM_HOME=~/.gem/ruby/2.0.0
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
 config_path=~/.ZAP_D/config.xml
-zap_path=~/ZAP_Dev_Build/zap.sh
+zap_path=/usr/local/ZAP_Dev_Build/zap.sh
 
-time ~/workspace/automated-scanning/bin/run_scan.pl \
+git_update() {
+	pushd $1
+	git pull origin master
+	popd
+}
+
+git_update ~/workspace/mediawiki
+git_update ~/workspace/mediawiki-extensions/MobileFrontend
+
+time ~/workspace/wikimedia-security-automated-scanning/bin/run_scan.pl \
 	-xH \
-	-c $config_path \
+	-c ~/.wmf.conf \
+	-C $config_path \
 	-p $zap_path \
-	-b ~/workspace/mediawiki-core/tests/browser \
+	-b ~/workspace/mediawiki/tests/browser \
 	beta_en
 
-time ~/workspace/automated-scanning/bin/run_scan.pl \
+time ~/workspace/wikimedia-security-automated-scanning/bin/run_scan.pl \
 	-xH \
-	-c $config_path \
+	-c ~/.wmf.conf \
+	-C $config_path \
 	-p $zap_path \
-	-b ~/workspace/extensions/MobileFrontend/tests/browser \
+	-b ~/workspace/mediawiki-extensions/MobileFrontend/tests/browser \
 	beta_en_mobile
 
