2ee3b8f98eabf173bca46e3deb8ade78949596a0
Darian Anthony Patrick <dpatrick@wikimedia.org>
2016-01-08 15:42:36 -0800

    Add Flow and VisualEditor scanning
    Change-Id: I4759c2fbe36a0d82b52535e9f06f0da3ee61e7ef

--- bin/scan_from_cron.sh
+++ bin/scan_from_cron.sh
@@ -11,8 +11,24 @@ git_update() {
 	popd
 }
 
-git_update ~/workspace/mediawiki
-git_update ~/workspace/mediawiki-extensions/MobileFrontend
+bundle_install() {
+	pushd $1
+	bundle install --path ~/gems/2.0.0
+	popd
+}
+
+repos=(
+	~/workspace/mediawiki
+	~/workspace/mediawiki-extensions/MobileFrontend
+	~/workspace/mediawiki-extensions/Flow
+	~/workspace/mediawiki-extensions/VisualEditor
+)
+
+for repo in "${repos[@]}"
+do
+	git_update $repo
+	bundle_install $repo
+done
 
 time ~/workspace/wikimedia-security-automated-scanning/bin/run_scan.pl \
 	-xH \
@@ -29,4 +45,20 @@ time ~/workspace/wikimedia-security-automated-scanning/bin/run_scan.pl \
 	-p $zap_path \
 	-b ~/workspace/mediawiki-extensions/MobileFrontend/tests/browser \
 	beta_en_mobile
+
+time ~/workspace/wikimedia-security-automated-scanning/bin/run_scan.pl \
+	-xH \
+	-c ~/.wmf.conf \
+	-C $config_path \
+	-p $zap_path \
+	-b ~/workspace/mediawiki-extensions/Flow/tests/browser \
+	beta_en
+
+time ~/workspace/wikimedia-security-automated-scanning/bin/run_scan.pl \
+	-xH \
+	-c ~/.wmf.conf \
+	-C $config_path \
+	-p $zap_path \
+	-b ~/workspace/mediawiki-extensions/VisualEditor/modules/ve-mw/tests/browser \
+	beta_en
 
